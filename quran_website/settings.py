739958dc4ca2d64f4f58353c1e3af093cdd72256
ayazkhattakup <ayazkhattakupwork@gmail.com>
2024-06-05 17:49:45 +0500

    added IP Address

--- quran_website/settings.py
+++ quran_website/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 
 PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID', default='default_value')
 
-ALLOWED_HOSTS = ['http://51.20.1.154','localhost']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
